aaaee4f86bb0b056753e844a51e97810c5b1c5cc
Chris Normansell <chris.normansell@gmail.com>
2017-03-14 12:37:16 +0000

    fix for breaking iOS header change in RN 0.40.0 (#4)
    * fix for breaking iOS header change in RN 0.40.0
    
    * Update RNVersionNumber.h
    
    Backward compatibility change

--- ios/RNVersionNumber.h
+++ ios/RNVersionNumber.h
@@ -1,5 +1,8 @@
-
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
 #import "RCTBridgeModule.h"
+#endif
 
 @interface RNVersionNumber : NSObject <RCTBridgeModule>
 
