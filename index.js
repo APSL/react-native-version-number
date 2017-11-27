be29fd7e9661f71fd1c988511f732d58244b01cd
Valentin Tsatskin <val@valtsatskin.com>
2017-11-27 14:59:29 -0500

    update flow types for potential void

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ import { NativeModules } from 'react-native';
 const { RNVersionNumber } = NativeModules;
 
 type VersionObject = {
-  appVersion: string,
-  buildVersion: string
-}
+  appVersion: ?string,
+  buildVersion: ?string,
+  bundleIdentifier: ?string
+};
 
 const VersionNumber: VersionObject = {
   appVersion: RNVersionNumber && RNVersionNumber.appVersion,
   buildVersion: RNVersionNumber && RNVersionNumber.buildVersion,
   bundleIdentifier: RNVersionNumber && RNVersionNumber.bundleIdentifier
-}
+};
 
 export default VersionNumber;
