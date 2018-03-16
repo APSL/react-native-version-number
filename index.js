6dc792d955bebe896454037063183b7865065a3c
Valentin Tsatskin <val@valtsatskin.com>
2018-03-16 11:47:40 +0300

    ensure RNVersionNumber exists for unit tests (with updated flow types) (#17)
    * ensure RNVersionNumber exists for unit tests
    
    Without this unit tests will break with Jest
    
    * update flow types for potential void

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ import { NativeModules } from 'react-native';
 const { RNVersionNumber } = NativeModules;
 
 type VersionObject = {
-  appVersion: string,
-  buildVersion: string,
-  bundleIdentifier: string,
+  appVersion: ?string,
+  buildVersion: ?string,
+  bundleIdentifier: ?string
 };
 
 const VersionNumber: VersionObject = {
-  appVersion: RNVersionNumber.appVersion,
-  buildVersion: RNVersionNumber.buildVersion,
-  bundleIdentifier: RNVersionNumber.bundleIdentifier,
+  appVersion: RNVersionNumber && RNVersionNumber.appVersion,
+  buildVersion: RNVersionNumber && RNVersionNumber.buildVersion,
+  bundleIdentifier: RNVersionNumber && RNVersionNumber.bundleIdentifier
 };
 
 export default VersionNumber;
